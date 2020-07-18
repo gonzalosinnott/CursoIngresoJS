ef0a055abed39367e940db532f8a088fbaeee9ac
gonzalosinnott <gonzalo.sinnott@gmail.com>
2020-07-18 20:41:52 -0300

    18/07

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -2,13 +2,28 @@
 si está entre las 7 y las 11 : "Es de mañana.".
 */
 
-function mostrar()
+function mostrar() 
 {
-	var horaDelDia = txtIdHora.value;
-	horaDelDia = parseInt(horaDelDia);
+  var horaDelDia = txtIdHora.value;
+  horaDelDia = parseInt(horaDelDia);
 
-	if (horaDelDia > 6 && horaDelDia < 12)
-	{
-		alert ("Es de mañana");
-	}
+  switch (horaDelDia) 
+  {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      alert ("Es de mañana.");
+      break;
+    default:
+      break;
+  }
 }
+
+/*
+if (horaDelDia > 6 && horaDelDia < 12) 
+{
+  alert("Es de mañana");
+}
+*/
--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,23 @@
+/*Al selecionar un destino , indicar el punto cardinal de nuestro pais 
+en donde se encuentra Norte, Sur, Este u Oeste */
+
 function mostrar()
 {
 	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
-
-}//FIN DE LA FUNCIÓN
+	
+	switch(destinoIngresado)
+	{
+		case "Bariloche":
+			alert ("Se encuentra al Sur");
+			break;
+		case "Cataratas":
+			alert ("Se encuentra al Norte");
+			break;
+		case "Mar del plata":
+			alert ("Se encuentra al Este");
+			break;
+		case "Cordoba":
+			alert ("Se encuentra al Oeste");
+			break;
+	}
+}
